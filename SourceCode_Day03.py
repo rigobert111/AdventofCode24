929a3326251d79e23632ab5bd01300f7610aa742
rigobert111 <martinboettcherpoehlde@t-online.de>
2025-01-01 17:08:20 +0100

    Add files via upload

---
+++ SourceCode_Day03.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 16:29:49 2025
+
+@author: VW1WBMY
+"""
+
+with open('C:/Users/VW1WBMY/AdventofCode/Day03/input_data.txt') as file:
+    data = file.read()
+#print(data)
+
+result = 0
+
+for a in range(1, 999):
+    for b in range(1, 999):
+        word = "mul({},{})".format(a,b)
+        occurence = data.count(word)
+        result+=a*b*occurence
+
+print(result)
